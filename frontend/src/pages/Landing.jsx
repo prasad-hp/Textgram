246437d553e1457bcab056478bf4bf767830105b
Prasad Hp <prasadhp@outlook.in>
2024-05-03 01:23:32 +0530

    Fixed the dragging in textbox at Create post

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -38,8 +38,8 @@ function LandingPage() {
             <div className="flex flex-col align-start w-screen lg:w-1/3 md:w-1/2 max-w-xl">
                 <div className="w-full border rounded-lg my-2 p-3">
                     <form onSubmit={handleSubmit} className="flex">
-                        <textarea placeholder="Click here to create new Post" className="justify-start w-10/12" value={textInput} onChange={handleInputChange}></textarea>
-                        <button className="justify-end align-bottom w-2/12 bg-blue-600 mx-3 my-1.5 rounded-xl" type="submit">Post</button>
+                        <input placeholder="Click here to create new Post" className="justify-start w-10/12" value={textInput} onChange={handleInputChange} type="text"></input>
+                        <button className="justify-end align-bottom w-2/12 bg-blue-600 mx-3 my-1.5 rounded-xl h-8" type="submit">Post</button>
                     </form>
                 </div>
                 {postList}
