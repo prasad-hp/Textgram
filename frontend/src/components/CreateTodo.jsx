3e417d370b86525e72c1c49d98ebc57285c0a265
Prasad Hp <prasadhp@outlook.in>
2024-05-05 23:21:11 +0530

    Post request is done

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function CreateTodo(){
+    const [inputText, setInputText] = useState("")
+
+    function handleChange(event){
+        setInputText(event.target.value)
+    }
+
+    async function handleSubmit(){
+        try {
+            await axios({
+                method:"post",
+                url:"http://localhost:3001/post",
+                data:{
+                    postText: inputText
+                }
+            },
+        {headers:{
+            "Content-Type": "application/json"
+        }})
+        } catch (error) {
+            console.error(error.message)
+        }
+
+
+    }
     return(
-        <form>
-            <input type="text" className="border-2 border-black rounded-md"/> 
+        <form onSubmit={handleSubmit}>
+            <input type="text" className="border-2 border-black rounded-md" value={inputText} onChange={handleChange}/> 
             <input type="submit" className="border-2 hover:cursor-pointer rounded-md"/> 
         </form>
     )
