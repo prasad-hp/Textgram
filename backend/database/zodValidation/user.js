f6059160fb6d51fb54b8477d507146e21096c9b4
Prasad Hp <prasadhp@outlook.in>
2024-05-13 15:50:57 +0530

    Created the login function

--- backend/database/zodValidation/user.js
+++ backend/database/zodValidation/user.js
@@ -1,10 +1,14 @@
 import zod from "zod";
 
-const userSchema = zod.object({
+const signupSchema = zod.object({
     firstName: zod.string(),
     lastName: zod.string(),
     email: zod.string().email(),
     password: zod.string().min(6, {message:"Password must be minimun 6 characters"})
 })
+const loginSchema = zod.object({
+    email:zod.string().email(),
+    password: zod.string().min(6, {message: "Password must be min 6 charaters"})
+})
 
-export default userSchema;
+export {signupSchema, loginSchema};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express"
 import User from "../database/user.js"
-import userSchema from "../database/zodValidation/user.js"
+import {signupSchema, loginSchema} from "../database/zodValidation/user.js"
 import JWT_SECRET from "../config.js"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
@@ -8,10 +8,10 @@ import bcrypt from "bcrypt"
 const router = express.Router()
 router.use(express.json())
 
-router.post("/create", async(req, res)=>{
+router.post("/signup", async(req, res)=>{
     try {
         const createUser = req.body
-        const parsedUser = userSchema.safeParse(createUser)
+        const parsedUser = signupSchema.safeParse(createUser)
         if(!parsedUser.success){
             return res.status(400).json(parsedUser.error.issues[0].message)
         }
@@ -30,12 +30,37 @@ router.post("/create", async(req, res)=>{
         const token = jwt.sign({tokenInput}, JWT_SECRET)
 
         res.status(200).json({
-            message:"User Created Successfully",
+            message:`${createUser.firstName} your account created Successfully`,
             data:token
         })
         
     } catch (error) {
         res.status(500).json(error.message)
+    }
+})
+router.post("/login", async(req, res)=>{
+    try {
+        const loginUser = req.body;
+        const parsedUser = loginSchema.safeParse(loginUser)
+        if(!parsedUser.success){
+            return res.status(400).json(parsedUser.error.issues[0].message)
+        }
+        const findUser = await User.findOne({
+            email: loginUser.email
+        })
+        const loginPassword = await bcrypt.compare(loginUser.password, findUser.password)
+        if(!loginPassword){
+            return res.status(400).json({message:"Please enter valid password"})
+        }
+        const tokenInput = loginUser.password;
+        const token = jwt.sign(tokenInput, JWT_SECRET)
+        res.status(200).json({
+            message:"You Are successfully logged in.",
+            token: token
+        })
+    } catch (error) {
+        res.status(500).json(error.message)
+        
     }
 })
 
